668cac76a15f6a9641d4c97fedb1ecc8d1e1dc2e
Niklas Bürkner <niklasbuerkner6@gmail.com>
2025-09-08 11:56:21 +0200

    Euler6 Lösung+ comment changes

--- src/Euler6.java
+++ src/Euler6.java
@@ -1,31 +1,28 @@
 public class Euler6 {
 
+    /**
+     * sumSquareWithoutBrackets() at first the SumSquare of each number and then the Addition to each
+     * sumSquaresInBrackets() the result gets multiplicated by itself
+     *
+     * @return returns difference between sumSquareInBrackets and sumSquareWithoutBrackets
+     */
+    public int sumSquaresResult() {
+        return sumSquaresInBrackets() - sumSquareWithoutBrackets();
+    }
 
-
-    private int sumSquareWithoutBrackets(){
+    private int sumSquareWithoutBrackets() {
         int sumSquareCalculate = 0;
         for (int x = 1; x <= 100; x++) {
-            sumSquareCalculate += (x * x);
+            sumSquareCalculate += x * x;
         }
         return sumSquareCalculate;
     }
 
-    private int sumSquaresInBrackets(){
+    private int sumSquaresInBrackets() {
         int sumSquareCalculateNumbers = 0;
         for (int x = 1; x <= 100; x++) {
             sumSquareCalculateNumbers += x;
         }
         return sumSquareCalculateNumbers * sumSquareCalculateNumbers;
     }
-
-    /**
-     * sumSquareWithoutBrackets() at first the SumSquare of each number and then the Addition to each
-     * sumSquaresInBrackets() the result gets multiplicated by itself
-     * @return returns difference between sumSquareInBrackets and sumSquareWithoutBrackets
-     */
-    public int sumSquaresResult(){
-        return sumSquaresInBrackets() - sumSquareWithoutBrackets();
-    }
-
-
 }
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@ import org.w3c.dom.ls.LSOutput;
 public class Main {
 
     public static void main(String[] args) {
-        Euler5 Euler5Object = new Euler5();
-
-        //Gibt Euler6 aus
         Euler6 Euler6Object = new Euler6();
         System.out.println(Euler6Object.sumSquaresResult());
     }
